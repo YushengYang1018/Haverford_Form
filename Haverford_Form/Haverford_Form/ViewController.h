2be610c51e9ab76749558931ead84bd12cd7fb02
tonylibra <tonylibra1018@gmail.com>
2015-12-21 14:38:24 -0500

    pod init, add GoogleSignIn, Parse, PayPal, and BFPaperButton SDKs

---
+++ Haverford_Form/Haverford_Form/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Haverford_Form
+//
+//  Created by YangYusheng on 12/21/15.
+//  Copyright © 2015 softwaremerchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Haverford_Form/Haverford_Form/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Haverford_Form
+//
+//  Created by YangYusheng on 12/21/15.
+//  Copyright © 2015 softwaremerchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
